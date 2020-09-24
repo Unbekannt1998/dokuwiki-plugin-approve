46fd00b46f1fba594b87345763c6703396003879
Unbekannt <unbekannt-gitlab@protonmail.com>
2020-09-24 15:10:16 +0200

    prevent self approval

--- conf/metadata.php
+++ conf/metadata.php
@@ -1,6 +1,7 @@
 <?php
 $meta['no_apr_namespaces'] = array('string');
 $meta['no_apr_groups'] = array('string');
+$meta['self_approve'] = array('onoff');
 $meta['prettyprint'] = array('onoff');
 $meta['ready_for_approval'] = array('onoff');
 $meta['display_banner_for_readonly'] = array('onoff');
