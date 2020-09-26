93fd5733ea3b5f91d11bbe452001498da9370b25
Unbekannt <unbekannt-gitlab@protonmail.com>
2020-09-26 23:45:52 +0200

    add Settings info

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -2,7 +2,8 @@
 $lang['no_apr_namespaces'] = 'Namensräume, wo dieses Plugin <strong>NICHT</strong> gelten soll (mit Leerzeichen sepriert)';
 $lang['no_apr_groups'] = 'Gruppen oder Benutzer die keine Freigabe brauchen (mit Leerzeichen sepriert)';
 $lang['self_approve'] = 'Erlaube selbst Freigabe';
-$lang['allow_self_approve'] = 'Eine Liste mit Benutztern und Gruppen die weiterhin selbst frei geben können (space seperatet list)';
+$lang['allow_self_approve'] = 'Eine Liste mit Benutztern und Gruppen die weiterhin selbst frei geben können (mit Leerzeichen sepriert)';
+$lang['use_discord_notify'] = 'Den Aktuellen Status mit dem discordnotifier Plugin mitteilen';
 $lang['prettyprint'] = 'Ausdruck mit schöner Überschrift';
 $lang['ready_for_approval'] = 'Zwischenstatus "Fertig zur Freigabe" aktivieren.';
 $lang['strict_approver'] = 'Nur Freigabeberechtigte können ihre Seiten freigeben (ausschließlich).';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -3,6 +3,7 @@ $lang['no_apr_namespaces'] = 'Namespaces this plugin <strong>does not</strong> a
 $lang['no_apr_groups'] = 'Groups or Users that do not need any approval (space separated list)';
 $lang['self_approve'] = 'Allow self approve';
 $lang['allow_self_approve'] = 'A list of Users and Groups that still can self approve (space seperatet list)';
+$lang['use_discord_notify'] = 'Update the current State of the page with the discordnotifier plugin';
 $lang['prettyprint'] = 'Use pretty headings on print.';
 $lang['ready_for_approval'] = 'Enable intermediate state "mark ready for approval" to let a reviewer know the page can be checked/approved';
 $lang['strict_approver'] = 'Only approvers can approve their pages.';
